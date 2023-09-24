69e7e97df58e0576b1b06a2ece4600cdba08c410
Yusuf <yemmola@yahoo.com>
2023-09-24 14:03:07 +0200

    Added lesson_2.py file

---
+++ lesson_2.py
@@ -0,0 +1,8 @@
+#Home work 2 pn Python variables
+# Define the variables
+a = 5
+b = 3
+c = 10
+# Compute and print the logical output
+print(a + b * c == 80) # This produce false
+print(a / b + c == 5 / 13) # This also produce false and 11.67 as the answer instead
