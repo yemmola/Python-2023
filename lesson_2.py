36140c7e9fc17a9d9b2a6a05d7556cfcf82242b5
Yusuf <yemmola@yahoo.com>
2023-09-24 20:31:07 +0200

    Added lesson_2.py file

--- lesson_2.py
+++ lesson_2.py
@@ -6,3 +6,7 @@ c = 10
 # Compute and print the logical output
 print(a + b * c == 80) # This produce false
 print(a / b + c == 5 / 13) # This also produce false and 11.67 as the answer instead
+
+#After creating another branch, i repeat the task as follows:
+print((a + b) * c == 80) # This produce True now
+print(a / (b + c) == 5 / 13) # This also true as well
