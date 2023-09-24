11dfd9b77e63db24babfda8a5e730f6aab8c061d
Yusuf <yemmola@yahoo.com>
2023-09-24 21:13:15 +0200

    Added lesson_2.py file

--- lesson_2.py
+++ lesson_2.py
@@ -1,12 +1,18 @@
-#Home work 2 pn Python variables
+# Home work 2 pn Python variables
+# Task1
 # Define the variables
 a = 5
 b = 3
 c = 10
 # Compute and print the logical output
-print(a + b * c == 80) # This produce false
-print(a / b + c == 5 / 13) # This also produce false and 11.67 as the answer instead
+print(a + b * c == 80)  # This produce false
+print(a / b + c == 5 / 13)  # This also produce false and 11.67 as the answer instead
 
-#After creating another branch, i repeat the task as follows:
-print((a + b) * c == 80) # This produce True now
-print(a / (b + c) == 5 / 13) # This also true as well
+# TAsk 2
+# After creating another branch, i repeat the task as follows:
+print((a + b) * c == 80)  # This produce True now
+print(a / (b + c) == 5 / 13)  # This also true as well
+
+##Task: Kinting and formatter
+# using ctrl+shift +M, No problems have been detected  in workspacelesoo
+# using black, typed "black lesson_2.py"
